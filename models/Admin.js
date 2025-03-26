ee7e5cf9a736655e102b3ee6c0caf60c3a7b3e33
santhosh611 <santhoshsekar999@gmail.com>
2025-03-26 20:13:11 +0530

    fix:hashpassword

--- models/Admin.js
+++ models/Admin.js
@@ -34,18 +34,6 @@ const adminSchema = mongoose.Schema(
   }
 );
 
-// Hash password before saving
-adminSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
 
-// Compare password method
-adminSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 module.exports = mongoose.model('Admin', adminSchema);
